ad58a37829e8cae9d84e16d40663b45438205178
Mustafa Aleem <maleem@intralinks.com>
2023-03-07 17:35:54 +0000

    Added weather service

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  env:"production",
+  apiUrl: 'https://api.openweathermap.org/data/2.5/',
+  apiKey: '44508996932a6e3747e9b157bb3a2f52'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  env:"base",
+  apiUrl: 'https://api.openweathermap.org/data/2.5/',
+  apiKey: '44508996932a6e3747e9b157bb3a2f52'
+};
+
