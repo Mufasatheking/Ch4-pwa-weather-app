534742f1007f416c2c2fc38c8aa5f24be912b1fb
Mustafa Aleem <maleem@intralinks.com>
2023-03-07 17:44:02 +0000

    Got basic app working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
+import { WeatherComponent } from './weather/weather.component';
+import {MatCardModule} from "@angular/material/card";
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    MatIconModule,
+    MatInputModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {environment} from "../environments/environment";
+import {Weather} from "./weather";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +10,13 @@ import {HttpClient} from "@angular/common/http";
 export class WeatherService {
 
   constructor(private http: HttpClient) { }
+  getWeather(city: string): Observable<Weather> {
+    const options = new HttpParams()
+      .set('units', 'metric')
+      .set('q', city)
+      .set('appId', environment.apiKey);
+
+    return this.http.get<Weather>(environment.apiUrl + 'weather', { params: options });
+
+  }
 }
