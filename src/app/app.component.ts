a8d8c2368dec3a208f021a20169a566a9c9875b4
Mustafa Aleem <maleem@intralinks.com>
2023-03-08 01:13:12 +0000

    Files I forgot to stage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {filter, map, switchMap, tap} from "rxjs";
+
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {SwUpdate} from "@angular/service-worker";
 import {environment} from "../environments/environment";
 
 @Component({
@@ -6,7 +10,51 @@ import {environment} from "../environments/environment";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'weather-app';
-  public environment = environment.env
+  constructor(private updates: SwUpdate, private snackbar: MatSnackBar) {}
+
+  ngOnInit() {
+    /* this.updates.available.pipe(
+      switchMap(() => this.snackbar.open('A new version is available!', 'Update now').afterDismissed()),
+      tap(p => console.log("first")),
+      filter(result => result.dismissedByAction),
+      tap(p => console.log("second")),
+      map(() => this.updates.activateUpdate().then(() => {console.log("updated");location.reload()}))).subscribe();
+  } */
+  this.updates.versionUpdates.pipe(
+    tap(p => console.log("updates pipe 1"))
+    ).subscribe(evt => {
+      console.log(evt);
+      switch (evt.type) {
+        case 'VERSION_DETECTED':
+          console.log(`Downloading new app version: ${evt.version.hash}`);
+          break;
+        case 'VERSION_READY':
+          console.log(`Current app version: ${evt.currentVersion.hash}`);
+          console.log(`New app version ready for use: ${evt.latestVersion.hash}`);
+          this.snackbarStuff();
+          break;
+        case 'VERSION_INSTALLATION_FAILED':
+          console.log(`Failed to install app version '${evt.version.hash}': ${evt.error}`);
+          break;
+      }
+    });
+}
+
+ snackbarStuff(){
+  console.log('snackbarStuff1');
+  let sbref = this.snackbar.open('A new version is available!', 'Update now');
+  console.log('snackbarStuff2');
+  sbref.afterDismissed().subscribe(() => {
+    console.log('The snackbar was dismissed');
+    location.reload();
+  });
+ }
+ public openSnackBar(){
+  let sbref = this.snackbar.open('Test snackbar!', 'Action to close?');
+  sbref.afterDismissed().subscribe(() => {
+    console.log('The test snackbar was dismissed');
+  });
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,41 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, isDevMode } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { HeaderComponent } from './header/header.component';
 import {HttpClientModule} from "@angular/common/http";
-import { WeatherComponent } from './weather/weather.component';
+import {MatButtonModule} from "@angular/material/button";
 import {MatCardModule} from "@angular/material/card";
-import {MatInputModule} from "@angular/material/input";
 import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherComponent
+    WeatherComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     MatIconModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    MatSnackBarModule,
+    MatButtonModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
